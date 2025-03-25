86a79e2a113b82a334a90482a96d3ca168318658
Anthony Micciche <amicciche@umass.edu>
2025-03-25 05:47:05 -0400

    implemented saran-vazirani (intitial results not great - there might be a problem)

--- src/QuantumHamlet.jl
+++ src/QuantumHamlet.jl
@@ -5,6 +5,7 @@ using Graphs
 using GraphsFlows
 using GraphMakie
 using Colors
+using MultiwayNumberPartitioning, HiGHS
 
 greet() = print("(|To be> + |not to be>)/√2")
 
@@ -82,9 +83,9 @@ function naive_cost_of_partition(land::quantumLand, graph::Graphs.Graph)
     return cost, edgecolors
 end
 
-function k_partition_saran_vazirani(g::Graphs.Graph)
+function k_partition_saran_vazirani(g::Graphs.Graph, k)
     d = Graphs.DiGraph(g)
-    capacity_matrix = zeros(Int, 9, 9)
+    capacity_matrix = zeros(Int, nv(g), nv(g))
     for e in edges(d)
         u, v = src(e), dst(e)
         capacity_matrix[u, v] = 1
@@ -92,12 +93,89 @@ function k_partition_saran_vazirani(g::Graphs.Graph)
 
     min_cuts = []
     for e in edges(g)
-        push!(min_cuts, GraphsFlows.mincut(d,src(e),dst(e),capacity_matrix,GraphsFlows.BoykovKolmogorovAlgorithm()))
+        cut = GraphsFlows.mincut(d,src(e),dst(e),capacity_matrix,GraphsFlows.BoykovKolmogorovAlgorithm())
+        push!(min_cuts, cut)
     end
 
-    sort!(min_cuts, by = x-> x[3])
+    unique!(min_cuts)
+    sort!(min_cuts, by = x-> x[3], rev=false)
 
-    return min_cuts
+    edge_set = []
+    for c in min_cuts
+        push!(edge_set, QuantumHamlet.edges_spanning_partition(g,c[1],c[2]))
+    end
+
+    dump = []
+    g_prime = copy(g)
+    new_reg = Dict()
+    for cut in edge_set
+        for e in cut
+            rem_edge!(g_prime, e)
+        end
+    
+        components = Graphs.connected_components(g_prime)
+        S = [length(x) for x in components]
+        inds = partition(S, k; optimizer = HiGHS.Optimizer);
+        ans = partitionable(S, inds, k)
+        push!(dump, (copy(components), ans))
+
+        
+        if ans == true
+            # Turn the found solution into a dictionary/ vector of vectors (i.e. [[Nodes 1 4 5], [Nodes 2 3 6]])
+            for i in 1:k
+                for j in S[inds .== i]
+                    index = findfirst(length.(components).==j)
+                    chosen_nodes = components[index]
+                    deleteat!(components, index)
+                    for node in chosen_nodes
+                        new_reg[node] = i
+                    end
+                end
+            end 
+
+            break
+        end
+    end
+
+    return new_reg, dump
+end
+
+"""Helper function for Saran-Vazirani. 
+Takes an input to multipartion S, and indices returned by the optimizer, inds. 
+This function returns a bool stating whether the partiion is perfectly balanced or not."""
+function partitionable(S, inds, k)
+    ans = true
+    val = sum(S[inds .== 1])
+    for i in 2:k
+        val_prime = sum(S[inds .== i])
+        if val_prime != val
+            ans = false
+            break
+        end
+    end
+    return ans
+end
+
+"""Helper function for Saran-Vazirani. 
+Given a graph g, and two vectors corresponding the vertices within that graph, returns the set of edges the crosses the partition"""
+function edges_spanning_partition(g::Graphs.Graph, v1, v2)
+    if length(v1) > length(v2)
+        v3 = v2
+        v2 = v1
+        v1 = v3
+    end
+
+    edges = []
+    for v in v1
+        neighbors = Graphs.neighbors(g,v)
+        for n in neighbors
+            if n ∉ v1
+                push!(edges,Edge(v,n))
+            end
+        end
+    end
+
+    return edges
 end
 
 function Base.show(io::IO, land::quantumLand)
--- src/test.jl
+++ src/test.jl
@@ -11,17 +11,6 @@ function random_graphstate(n::Int)
     return QuantumClifford.graphstate(rand_stab)
 end
 
-numVillages = 3
-logicalBitsPerVillage = 4
-reg = QuantumHamlet.defaultVillagerRegistry(numVillages,logicalBitsPerVillage)
-
-land = quantumLand(reg, numVillages, numVillages*logicalBitsPerVillage)
-
-rand_graphstate = random_graphstate(logicalBitsPerVillage*numVillages)
-g = rand_graphstate[1]
-
-cost, edgecolors = QuantumHamlet.naive_cost_of_partition(land,g)
-
 function distinct_colors(n::Int)
     # Generate `n` distinct colors using HSV space
     return [HSV(i * 360 / n, 0.8, 0.9) for i in 0:n-1] .|> RGB
@@ -38,6 +27,29 @@ function print_graph(g::Graph; edgecolors=nothing, nodecolors=nothing)
     return f
 end
 
+#### Init
+numVillages = 3
+logicalBitsPerVillage = 8
+reg = QuantumHamlet.defaultVillagerRegistry(numVillages,logicalBitsPerVillage)
+rand_graphstate = random_graphstate(logicalBitsPerVillage*numVillages)
+g = rand_graphstate[1]
 village_colors = distinct_colors(numVillages)
+
+#### Random initial labeling 
+land = quantumLand(reg, numVillages, numVillages*logicalBitsPerVillage)
+
+cost, edgecolors = QuantumHamlet.naive_cost_of_partition(land,g)
+
 nodecolors = [village_colors[reg[i]] for i in 1:numVillages*logicalBitsPerVillage]
 f = print_graph(g, edgecolors=edgecolors, nodecolors=nodecolors)
+
+#### Approximation algorithm for balanced k partitioning
+new_reg, dump = QuantumHamlet.k_partition_saran_vazirani(g, numVillages)
+
+# TODO instead of creating a new land, maybe add a function to relabel qubits? maybe?
+improvedLand = quantumLand(new_reg, numVillages, numVillages*logicalBitsPerVillage)
+
+cost2, edgecolors2 = QuantumHamlet.naive_cost_of_partition(improvedLand,g)
+
+nodecolors2 = [village_colors[new_reg[i]] for i in 1:numVillages*logicalBitsPerVillage]
+f2 = print_graph(g, edgecolors=edgecolors2, nodecolors=nodecolors2)
