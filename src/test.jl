545dd13b12c26a8dc5ebd2f3dfed299bf7ef5e44
Anthony Micciche <amicciche@umass.edu>
2025-07-14 12:42:41 +0200

    added KL algo to some test cases

--- src/test.jl
+++ src/test.jl
@@ -14,12 +14,12 @@ function random_graphstate(n::Int)
 end
 
 #### Init
-numVillages = 3
-logicalBitsPerVillage = 12
+numVillages = 2
+logicalBitsPerVillage = 50
 #rand_graphstate = random_graphstate(logicalBitsPerVillage*numVillages)
 #g = rand_graphstate[1]
-g = grid([6,6])
-#g = random_regular_graph(numVillages*logicalBitsPerVillage, 4)
+#g = grid([10,10])
+g = random_regular_graph(numVillages*logicalBitsPerVillage, 4)
 
 #### n/2 approximation algorithm for balanced k partitioning
 saran_reg, _ = QuantumHamlet.k_partition_saran_vazirani(g, numVillages)
@@ -94,54 +94,69 @@ function grid_graph(numVertices)
     return Graphs.grid([a,b])
 end
 
-function compare_partition_methods(graph_generator, numVillages; numSamples=20)
+function compare_partition_methods(graph_generator, numVillages; numSamples=100)
     pt = 4/3
     f = Figure(size=(900, 700),px_per_unit = 5.0, fontsize = 15pt)
 
     ax = f[1,1] = Axis(f[1,1],  xlabel="Logical Qubits per Village",ylabel="Required Bell pairs",title=string(numVillages)*" Villages on "*string(graph_generator))
     
-    sizes = 4:2:30
+    sizes = 4:4:52
     bury_costs = []
     bury_stds = []
-    bury_ghz_costs_optimistic = []
-    bury_ghz_costs_pessimistic = []
 
     saran_costs = []
     saran_stds = []
+
+    kl_costs = []
+    kl_stds = []
 
     randommin_costs = []
     randommin_stds = []
 
     for logicalBitsPerVillage in sizes
-        bury_sample_arr = []; bsa_cheap = []; bsa_expensive = [];
+        bury_sample_arr = []
         saran_sample_arr = []
         randommin_sample_arr = []
+        kl_sample_arr = []
         for _ in 1:numSamples
             g = graph_generator(numVillages*logicalBitsPerVillage)
 
             bury_reg = QuantumHamlet.bury_heuristic_global_v2(g, numVillages)
             buryLand = quantumLand(bury_reg, numVillages, numVillages*logicalBitsPerVillage)
             bury_cost, _ = QuantumHamlet.matching_cost_of_partition(buryLand, g)
-            cheap, expen = QuantumHamlet.vertex_cover_cost_of_partition(buryLand,g)
             push!(bury_sample_arr, bury_cost)
-            push!(bsa_cheap, cheap)
-            push!(bsa_expensive,expen)
 
             saran_reg, _ = QuantumHamlet.k_partition_saran_vazirani(g, numVillages)
             saranLand = quantumLand(saran_reg, numVillages, numVillages*logicalBitsPerVillage)
             saran_cost, _ = QuantumHamlet.matching_cost_of_partition(saranLand, g)
             push!(saran_sample_arr, saran_cost)
 
+            py_g = nx.Graph()
+            py_g.add_edges_from([(src(e), dst(e)) for e in edges(g)])
+            kl_part = nxcomm.kernighan_lin.kernighan_lin_bisection(py_g, partition=nothing, max_iter=10)
+            a, b = pyconvert(Vector{Int}, kl_part[0]), pyconvert(Vector{Int}, kl_part[1])
+            kl_reg = Dict()
+            for i in a
+                kl_reg[i] = 1
+            end
+            for i in b
+                kl_reg[i] = 2
+            end
+            klLand = quantumLand(kl_reg, numVillages, numVillages*logicalBitsPerVillage)
+            kl_cost, _ = QuantumHamlet.matching_cost_of_partition(klLand, g)
+            push!(kl_sample_arr, kl_cost)
+
             _, _, random_costs = random_sample(g, numVillages, logicalBitsPerVillage, method=QuantumHamlet.matching_cost_of_partition, vis=false)
             push!(randommin_sample_arr, minimum(random_costs))
         end
         push!(bury_costs, mean(bury_sample_arr))
         push!(bury_stds, std(bury_sample_arr))
-        push!(bury_ghz_costs_optimistic, mean(bsa_cheap))
-        push!(bury_ghz_costs_pessimistic, mean(bsa_expensive))
 
         push!(saran_costs, mean(saran_sample_arr))
         push!(saran_stds, std(saran_sample_arr))
+
+        push!(kl_costs, mean(kl_sample_arr))
+        push!(kl_stds, std(kl_sample_arr))
 
         push!(randommin_costs, mean(randommin_sample_arr))
         push!(randommin_stds, std(randommin_sample_arr))
@@ -149,11 +164,11 @@ function compare_partition_methods(graph_generator, numVillages; numSamples=20)
     scatter!(ax, sizes, bury_costs, color=:blue, marker=:circle)
     errorbars!(ax, sizes, bury_costs, bury_stds, color = :blue, whiskerwidth=10)
 
-    scatter!(ax, sizes, bury_ghz_costs_optimistic, color=:blue, marker=:utriangle)
-    scatter!(ax, sizes, bury_ghz_costs_pessimistic, color=:blue, marker=:cross)
-
     scatter!(ax, sizes, saran_costs, color=:green, marker=:circle)
     errorbars!(ax, sizes, saran_costs, saran_stds, color = :green, whiskerwidth=10)
+
+    scatter!(ax, sizes, kl_costs, color=:purple, marker=:circle)
+    errorbars!(ax, sizes, kl_costs, kl_stds, color = :purple, whiskerwidth=10)
 
     scatter!(ax, sizes, randommin_costs, color=:orange, marker=:circle)
     errorbars!(ax, sizes, randommin_costs, randommin_stds, color = :orange, whiskerwidth=10)
@@ -161,10 +176,8 @@ function compare_partition_methods(graph_generator, numVillages; numSamples=20)
     lines!(ax, [0,0], [0,0], label="n(k-1)/2 Bound", color=:black)
     lines!(ax, [0,0], [0,0], label="Best partition found\nover 50 random samples", color=:orange)
     lines!(ax, [0,0], [0,0], label="Saran Vazirani n/2\napprox for edge min", color=:green)
+    lines!(ax, [0,0], [0,0], label="KL", color=:purple)
     lines!(ax, [0,0], [0,0], label="Bury heuristic - Global", color=:blue)
-
-    scatter!(ax, [0,0], [0,0], label="m party GHZ states\nhave cost m-1", color=:gray, marker=:cross)
-    scatter!(ax, [0,0], [0,0], label="Cheap GHZ states", color=:gray, marker=:utriangle)
 
     lines!(ax, [0, maximum(sizes)], [0, (maximum(sizes)*numVillages)*(numVillages-1)/2], color=:gray)
  
@@ -208,3 +221,25 @@ function compare_generation_methods(graph_generator, numVillages)
     f[1,2] = Legend(f, ax, "Graph Preparation method")
     return f
 end
+
+### KL portion
+using CondaPkg
+CondaPkg.add("networkx")
+using PythonCall
+nx = pyimport("networkx")
+nxcomm = pyimport("networkx.algorithms.community")
+
+py_g = nx.Graph()
+py_g.add_edges_from([(src(e), dst(e)) for e in edges(g)])
+kl_part = nxcomm.kernighan_lin.kernighan_lin_bisection(py_g, partition=nothing, max_iter=10)
+a, b = pyconvert(Vector{Int}, kl_part[0]), pyconvert(Vector{Int}, kl_part[1])
+
+kl_reg = Dict()
+for i in a
+    kl_reg[i] = 1
+end
+for i in b
+    kl_reg[i] = 2
+end
+klLand = quantumLand(kl_reg, numVillages, numVillages*logicalBitsPerVillage)
+f_kl, kl_cost = QuantumHamlet.visualize_graph_on_land(klLand, g, method=QuantumHamlet.matching_cost_of_partition)
